95f6378b5efc8fef627a8ab30ca07c18076f44ac
Miguel Villareal <migzvillareal27@gmail.com>
2026-04-22 17:34:31 +0800

    crit fix not done

--- src/player.c
+++ src/player.c
@@ -1,11 +1,13 @@
 #include "./include/player.h"
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct Player {
     int hp;
     int row;
     int potions;
-    int crit_count;
+    int miss_count; //pirang beses mo namiss
+    int crit_chance;
 };
 
 Player* player_create(int start_row) {
@@ -13,6 +15,8 @@ Player* player_create(int start_row) {
     p->hp = 100;
     p->row = start_row;
     p->potions = 3;
+    p->miss_count = 0;
+    p->crit_chance;
     return p;
 }
 
@@ -20,15 +24,21 @@ void player_move(Player* p, int direction) {
     p->row += direction;
 }
 
+bool checkCritical(int crit_chance) {
+    return (rand() % 100) < crit_chance;
+}
+
 void player_shoot(Player* shooter, Player* target) {
     if (shooter->row == target->row) {
-        if (shooter->crit_count == 3) {
-            target->hp -= 20;
+        srand(time(NULL));
+        if (checkCritical((shooter->crit_chance * shooter->miss_count)) ){
+            target->hp -= 30;
         } else {
-            target->hp -= 20
+            target->hp -= 20;
         }
     } else {
         shooter->hp -= 10;
+        shooter->miss_count += 1;
     }
 }
 
