047fe0dc68646a9190f9fa2f2c552df352070de4
Miguel Villareal <migzvillareal27@gmail.com>
2026-05-09 12:42:27 +0800

    ultimate.c ult implementation edit

--- src/ultimate.c
+++ src/ultimate.c
@@ -1,4 +1,5 @@
-#include "./include/ultimate.h"
+#include "ultimate.h"
+#include "player.h"
 #include <stdio.h>
 
 void ultimate_set(Player* p, UltimateType type) {
@@ -10,28 +11,22 @@ int ultimate_can_use(Player* p) {
 }
 
 void ultimate_execute(Player* user, Player* enemy) {
-
     if (!user->ultimate_ready)
         return;
 
     switch (user->ultimate_type) {
-
         case ULT_ONE_SHOT:
             ult_one_shot(user, enemy);
             break;
-
         case ULT_BARRAGE:
             ult_barrage(user, enemy);
             break;
-
         case ULT_DEFLECT:
             ult_deflect(user, enemy);
             break;
-
         case ULT_REVEAL:
             ult_reveal(user, enemy);
             break;
-
         default:
             return;
     }
@@ -41,34 +36,29 @@ void ultimate_execute(Player* user, Player* enemy) {
 
 // ULTIMATE: ONE SHOT ONE KILL
 void ult_one_shot(Player* user, Player* enemy) {
-
-    if (user->row == enemy->row) {
+    if (user->col == enemy->col) {
         enemy->hp = 0;
     } else {
-        user->hp -= 20; 
+        user->hp -= 20;
     }
 }
 
-
-// ULTIMATE: THREE ROW BARRAGE
+// ULTIMATE: THREE COLUMN BARRAGE
 void ult_barrage(Player* user, Player* enemy) {
-
-    int r = enemy->row;
-
-    if (user->row >= r - 1 && user->row <= r + 1) {
+    int c = enemy->col;
+    if (user->col >= c - 1 && user->col <= c + 1) {
         enemy->hp -= 15;
     }
 }
 
-
 // ULTIMATE: DEFLECT
 void ult_deflect(Player* user, Player* enemy) {
+    (void)enemy;
     user->is_deflecting = 1;
 }
 
-
 // ULTIMATE: REVEAL POSITION
 void ult_reveal(Player* user, Player* enemy) {
-
-    printf("[ULTIMATE REVEAL] Enemy is at row: %d\n", enemy->row);
+    (void)user;
+    (void)enemy;
 }
